d92344115b1879267e7b1e403e8bac83f65d233e
mzhang055 <mzhang055@gmail.com>
2025-10-15 19:00:23 -0400

    complete task 2

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -1,30 +1,60 @@
 package dogapi;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- * This BreedFetcher caches fetch request results to improve performance and
- * lessen the load on the underlying data source. An implementation of BreedFetcher
- * must be provided. The number of calls to the underlying fetcher are recorded.
- *
- * If a call to getSubBreeds produces a BreedNotFoundException, then it is NOT cached
- * in this implementation. The provided tests check for this behaviour.
- *
- * The cache maps the name of a breed to its list of sub breed names.
+ * A BreedFetcher that caches successful results to avoid redundant API calls.
+ * Failed lookups (exceptions) are NOT cached and will retry on subsequent calls.
  */
 public class CachingBreedFetcher implements BreedFetcher {
-    // TODO Task 2: Complete this class
-    private int callsMade = 0;
+    private final BreedFetcher fetcher;
+    private final Map<String, List<String>> cache;
+    private int callsMade;
+
+    /**
+     * Creates a new CachingBreedFetcher that wraps the given fetcher.
+     * @param fetcher the underlying BreedFetcher to use for actual API calls
+     */
     public CachingBreedFetcher(BreedFetcher fetcher) {
-
+        this.fetcher = fetcher;
+        this.cache = new HashMap<>();
+        this.callsMade = 0;
     }
 
+    /**
+     * Fetch the list of sub breeds for the given breed.
+     * Results are cached so subsequent calls for the same breed won't hit the API.
+     * Exceptions are NOT cached - failed breeds will be retried.
+     *
+     * @param breed the breed to fetch sub breeds for
+     * @return list of sub breeds for the given breed
+     * @throws BreedNotFoundException if the breed does not exist
+     */
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+        // Check if we have a cached result
+        if (cache.containsKey(breed)) {
+            return cache.get(breed);
+        }
+
+        // Not in cache, so make the actual API call
+        callsMade++;
+        List<String> result = fetcher.getSubBreeds(breed);
+
+        // Cache the successful result
+        cache.put(breed, result);
+
+        return result;
     }
 
+    /**
+     * Returns the number of actual calls made to the underlying fetcher.
+     * Cached results don't count as calls.
+     *
+     * @return the number of calls made to the underlying fetcher
+     */
     public int getCallsMade() {
         return callsMade;
     }
